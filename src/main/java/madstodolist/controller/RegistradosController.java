372cacf413b645f17c170f7923c30e626db5943d
JoseIgnacio22 <jian1@alu.ua.es>
2023-10-16 13:11:06 +0200

    Añadida nueva plantilla, método en controlador y test respecto a la descripción de usuario

--- src/main/java/madstodolist/controller/RegistradosController.java
+++ src/main/java/madstodolist/controller/RegistradosController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -27,5 +29,16 @@ public class RegistradosController {
         model.addAttribute("usuarios", usuarios);
 
         return "listaUsuarios";
+    }
+
+    @GetMapping("/registrados/{id}")
+    public String descripcionUsuario(@PathVariable(value="id") Long idUsuario, Model model, HttpSession session) {
+
+        UsuarioData usuario = usuarioService.findById(managerUserSession.usuarioLogeado());
+        UsuarioData usuario1 = usuarioService.findById(idUsuario);
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("usuario1", usuario1);
+
+        return "descripcionUsuario";
     }
 }
