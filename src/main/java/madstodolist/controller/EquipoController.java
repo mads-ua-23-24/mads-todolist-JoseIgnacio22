13aa7a390565c48e0028ead11a7a0108c8468f00
JoseIgnacio22 <jian1@alu.ua.es>
2023-11-06 19:13:35 +0100

    Tercer test - detalle de equipo

--- src/main/java/madstodolist/controller/EquipoController.java
+++ src/main/java/madstodolist/controller/EquipoController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -26,12 +27,17 @@ public class EquipoController {
     @Autowired
     ManagerUserSession managerUserSession;
 
+    private Long comprobarUsuarioLogeado() {
+        Long idUsuarioLogeado = managerUserSession.usuarioLogeado();
+        if (idUsuarioLogeado == null)
+            throw new UsuarioNoLogeadoException();
+        return idUsuarioLogeado;
+    }
+
     @GetMapping("/equipos")
     public String listadoEquipos(Model model, HttpSession session) {
-        Long idUsuario = managerUserSession.usuarioLogeado();
-        if (idUsuario == null)
-            throw new UsuarioNoLogeadoException();
-        UsuarioData usuario = usuarioService.findById(idUsuario);
+        Long idUsuarioLogeado = comprobarUsuarioLogeado();
+        UsuarioData usuario = usuarioService.findById(idUsuarioLogeado);
 
         List<EquipoData> equipos = equipoService.findAllOrdenadoPorNombre();
 
@@ -39,5 +45,19 @@ public class EquipoController {
         model.addAttribute("equipos", equipos);
 
         return "listaEquipos";
+    }
+
+    @GetMapping("/equipos/{id}")
+    public String detalleEquipo(Model model, @PathVariable(value="id") Long idEquipo) {
+        Long idUsuarioLogeado = comprobarUsuarioLogeado();
+        UsuarioData usuarioLogeado = usuarioService.findById(idUsuarioLogeado);
+
+        EquipoData equipo = equipoService.recuperarEquipo(idEquipo);
+        List<UsuarioData> usuarios = equipoService.usuariosEquipo(idEquipo);
+        model.addAttribute("logeado", usuarioLogeado);
+        model.addAttribute("equipo", equipo);
+        model.addAttribute("usuarios", usuarios);
+
+        return "descripcionEquipo";
     }
 }
--- src/test/java/madstodolist/controller/EquipoWebTest.java
+++ src/test/java/madstodolist/controller/EquipoWebTest.java
@@ -13,6 +13,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Map;
+
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Mockito.when;
@@ -60,6 +62,38 @@ public class EquipoWebTest {
                         containsString("<h2>Listado de equipos</h2>"),
                         containsString("Proyecto P1"),
                         containsString("Proyecto P2")
+                )));
+    }
+
+    @Test
+    public void detalleEquipoMuestraInformacionCorrecta() throws Exception {
+        // GIVEN
+        // Dos usuarios en la base de datos
+        UsuarioData usuario = new UsuarioData();
+        usuario.setEmail("user@ua");
+        usuario.setPassword("123");
+        usuario.setNombre("Usuario Ejemplo");
+        usuario = usuarioService.registrar(usuario);
+        // Y un equipo en la base de datos
+        EquipoData equipo = equipoService.crearEquipo("Proyecto P1");
+        Long equipoId = equipo.getId();
+
+        // Pertenece al equipo
+        equipoService.añadirUsuarioAEquipo(equipoId, usuario.getId());
+
+        // WHEN
+        // El usuario se logea
+        when(managerUserSession.usuarioLogeado()).thenReturn(usuario.getId());
+        String url = "/equipos/" + equipoId;
+
+        // THEN
+        this.mockMvc.perform(get(url))
+                .andExpect(content().string(allOf(
+                        containsString("Proyecto P1"),
+                        containsString("<th>Nombre</th>"),
+                        containsString(usuario.getNombre()),
+                        containsString("<th>Correo</th>"),
+                        containsString(usuario.getEmail())
                 )));
     }
 }
